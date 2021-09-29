b3abf95408324d3bac41bbb59a07781e79949b18
mnyuan <2025403985@qq.com>
2021-09-29 16:01:23 +0800

    master 3 commit

--- git-test/src/main/java/GitTest.java
+++ git-test/src/main/java/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("Hello git 444444444");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test");
     }
 }
