9163242800c001b4e520aded88b3fbc4406d8f03
mnyuan <2025403985@qq.com>
2021-09-29 15:35:58 +0800

    first 1 commit

---
+++ git-test/src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author: MNYuan
+ * @create: Created in 15:34 2021/9/29
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git 111111111");
+    }
+}
