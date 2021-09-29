64783b195b088559bcf15c8bf8ce595b8683fccd
mnyuan <2025403985@qq.com>
2021-09-29 15:40:05 +0800

    second 2 commit

--- git-test/src/main/java/GitTest.java
+++ git-test/src/main/java/GitTest.java
@@ -6,5 +6,6 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git 111111111");
+        System.out.println("Hello git 222222222");
     }
 }
